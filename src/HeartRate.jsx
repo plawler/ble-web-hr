75b4872007461b384e5ff39bac76b0555244430f
Paul Lawler <paullawler@Paul-Lawlers-MacBook-Pro.local>
2024-03-15 13:28:04 -0400

    initial commit of web ble with hr

---
+++ src/HeartRate.jsx
@@ -0,0 +1,60 @@
+import React, {Component} from "react";
+import { HeartRateSensor } from "./HeartRateSensor";
+
+export default class HeartRate extends Component {
+
+    characteristic;
+
+    constructor() {
+        super();
+        this.state = {hr: 0};
+        this.connect = this.connect.bind(this) // why? https://stackoverflow.com/questions/20279484/how-to-access-the-correct-this-inside-a-callback/20279485#20279485
+    }
+
+    handleHeartRateChanged(event) {
+        const heartRate = event.target.value.getUint8(1);
+        console.log('heart rate changed ' + heartRate);   
+        this.setState({heartRate});     
+        return heartRate;
+    }
+
+    connect() {
+        return navigator.bluetooth.requestDevice({ filters: [{ services: ['heart_rate'] }] })
+        .then(device => { return device.gatt.connect() })
+        .then(connection => { return connection.getPrimaryService('heart_rate')})
+        .then(server => { return server.getCharacteristic('heart_rate_measurement')})
+        .then(charac => {
+            this.characteristic = charac;             
+            console.log(this.characteristic);
+            return this.startNotifications2();             
+        })
+        .catch(error => { console.error(error); });
+    }
+
+    startNotificatons(characteristic) {        
+        return characteristic.startNotifications().then(_ => {
+            console.log("starting characteristic notifications");
+            characteristic.addEventListener('characteristicvaluechanged', this.handleHeartRateChanged.bind(this));        
+        }).catch(e => console.log(e));
+    }
+
+    startNotifications2() {
+        return this.startNotificatons(this.characteristic);
+    }
+
+    stopNotifications() {
+        return this.characteristic.stopNotifications();
+    }
+
+    render() {
+        // const hr = new HeartRateSensor();           
+        return(
+            <div id="heartRate">
+                <p>This is the heart rate component</p>
+                <button onClick={this.connect}>Find sensors</button>
+                <p>Heart rate changes...{this.state.heartRate}</p>
+            </div>
+        )
+    }
+
+}
